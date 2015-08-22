c54ad667fe8bd4be25890881a9a0652f9205a560
omnikrll <kfhohn@gmail.com>
2015-08-22 01:58:38 -0400

    restructuring dom, setting up textState schema

--- GameManager.js
+++
@@ -1,9 +0,0 @@
-var humans;
-
-(function() {
-	humans = [
-		new Human('Patry Garcia'),
-		new Human('Arymo Watson'),
-		new Human('Vidy Grichy')
-	];
-})();
--- js/GameManager.js
+++ js/GameManager.js
@@ -10,17 +10,17 @@ var crew;
 	];
 
 	// render them to the dom
-	for (var i=0,ii=crew.length; i<ii; i++) {
-		var human = crew[i];
-		console.log(human.name);
-		var list_item = $('<li></li>').attr('id', 'human-' + human.id);
+	// for (var i=0,ii=crew.length; i<ii; i++) {
+	// 	var human = crew[i];
+	// 	console.log(human.name);
+	// 	var list_item = $('<li></li>').attr('id', 'human-' + human.id);
 
-		var name = $('<span></span>').addClass('name').html(human.name);
-		list_item.append(name);
+	// 	var name = $('<span></span>').addClass('name').html(human.name);
+	// 	list_item.append(name);
 
-		var health = $('<span></span>').addClass('health').html(human.health);
-		list_item.append(health);
+	// 	var health = $('<span></span>').addClass('health').html(human.health);
+	// 	list_item.append(health);
 
-		$('#crew-roster').append(list_item);
-	}
+	// 	$('#crew-roster').append(list_item);
+	// }
 })();
